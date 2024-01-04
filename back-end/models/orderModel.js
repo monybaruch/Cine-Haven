c23d59270f7d483a3765ab9fb5e1d017fc1e8bc8
מוני ברוך <monybaruch@gmail.com>
2024-01-04 18:40:33 +0200

    adding the orderModel and userModel

---
+++ back-end/models/orderModel.js
@@ -0,0 +1,114 @@
+const mongoose = require('mongoose');
+
+const orderSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    orderItems: [
+      {
+        name: {
+          type: String,
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+        },
+        image: {
+          type: String,
+          required: true,
+        },
+        price: {
+          type: Number,
+          required: true,
+        },
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+          ref: 'Product',
+        },
+      },
+    ],
+    shippingAddress: {
+      address: {
+        type: String,
+        required: true,
+      },
+      city: {
+        type: String,
+        required: true,
+      },
+      postalCode: {
+        type: String,
+        required: true,
+      },
+      country: {
+        type: String,
+        required: true,
+      },
+    },
+    paymentMethod: {
+      id: {
+        type: String,
+      },
+      status: {
+        type: String,
+      },
+      update_time: {
+        type: String,
+      },
+      email_address: {
+        type: String,
+      },
+    },
+    prices: {
+      type: {
+        purchase: { type: Number, required: true, default: 0 },
+        rental: {
+          type: {
+            oneWeek: { type: Number, required: true, default: 0 },
+            twoWeeks: { type: Number, required: true, default: 0 },
+            oneMonth: { type: Number, required: true, default: 0 },
+          },
+          required: true,
+        },
+      },
+      required: true,
+    },
+    shippingPrice: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    isPaid: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    paidAt: {
+      type: Date,
+    },
+    isDelivered: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    deliveredAt: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+const Order = mongoose.model('Order', orderSchema);
+
+export default Order;
