5a36ac9b79b8f0e7e80343494747eec1c5c7e86d
מוני ברוך <monybaruch@gmail.com>
2024-01-11 15:15:29 +0200

    order folders

--- front-end/src/slices/cartSlice.js
+++ front-end/src/slices/cartSlice.js
@@ -17,14 +17,17 @@ const cartSlice = createSlice({
       }
       //calc the items price
       state.itemsPrice = state.cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0);
+
       //calc the shipping cost (if the total order is over 60$ then the shipping is free , else the shipping is 7$)
 
       state.shippingPrice = state.itemsPrice > 60 ? 0 : 7;
 
       // calc total price
+
       state.totalPrice = Number(state.itemsPrice) + Number(state.shippingPrice).toFixed(2);
 
       //save the state to local storage
+
       localStorage.setItem('cart', JSON.stringify(state));
     },
   },
