0d52c61f225a6a8cf5b310828935f1db5d0ec00e
מוני ברוך <monybaruch@gmail.com>
2024-01-14 17:42:22 +0200

    fixing the header

--- front-end/src/components/MainHeader.jsx
+++ front-end/src/components/MainHeader.jsx
@@ -1,8 +1,11 @@
-import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Navbar, Container, Nav, Badge } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import { RiShoppingCartFill } from 'react-icons/ri';
 import { FaCircleUser } from 'react-icons/fa6';
+import { useSelector } from 'react-redux';
 const MainHeader = () => {
+  const { cartItems } = useSelector((state) => state.cart);
+  console.log(cartItems);
   return (
     <header>
       <Navbar variant="dark" expand="md" collapseOnSelect className="primary-color">
@@ -21,6 +24,11 @@ const MainHeader = () => {
                 <Nav.Link className="text-light">
                   <RiShoppingCartFill />
                   &nbsp;Cart
+                  {cartItems.length > 0 && (
+                    <Badge pill bg="success" style={{ marginLeft: '10px', padding: '10px' }}>
+                      {cartItems.reduce((acc, total) => acc + total.quantity, 0)}
+                    </Badge>
+                  )}
                 </Nav.Link>
               </LinkContainer>
               <LinkContainer to="login">
