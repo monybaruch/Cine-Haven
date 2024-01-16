4d3029d1bb18aa359b9c5b7f74ee4a50feeeab0e
מוני ברוך <monybaruch@gmail.com>
2024-01-16 15:21:53 +0200

    adding the logic for the user registration in the backend

--- controllers/userController.js
+++ controllers/userController.js
@@ -38,7 +38,28 @@ const loginUser = asyncHandler(async (req, res) => {
 // @access Public
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.send('user register');
+  const { name, email, password } = req.body;
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already registered! try a different email');
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data! please try again!');
+  }
 });
 
 // @desc   Logout user and clearing the http cookie
@@ -46,7 +67,8 @@ const registerUser = asyncHandler(async (req, res) => {
 // @access Private
 
 const logoutUser = asyncHandler(async (req, res) => {
-  res.send('user logout');
+  res.cookie('jwt', '', { httpOnly: true, expires: new Date(0) });
+  res.status(200).json({ message: 'You have logged out!' });
 });
 
 // @desc   Get user profile
--- models/userModel.js
+++ models/userModel.js
@@ -22,9 +22,19 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next(); // if the password is not changed then move to the next middlewere
+  }
+  // hashing the password that the user entered before creating a the user in the db
+  const salt = await bcrypt.genSalt(8);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
 userSchema.methods.passMatch = async function (userPassword) {
   return await bcrypt.compare(userPassword, this.password);
 };
+
 const User = mongoose.model('User', userSchema);
 
 export default User;
