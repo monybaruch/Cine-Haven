f3b6babc436f2a8ba0b56a40ea6fe2c3e0e0f9b8
מוני ברוך <monybaruch@gmail.com>
2024-01-17 23:01:48 +0200

    fixing an error

--- front-end/src/slices/cartSlice.js
+++ front-end/src/slices/cartSlice.js
@@ -1,6 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { updateCart } from '../utils/cartUtil';
-
+import { updateCart } from '../Utils/cartUtil';
 const initialState = localStorage.getItem('cart')
   ? JSON.parse(localStorage.getItem('cart'))
   : { cartItems: [], shippingAddress: {}, paymentMethod: 'PayPal' };
