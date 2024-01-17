26f18a4ee2de05b10e754612477d39c7b3655cdd
מוני ברוך <monybaruch@gmail.com>
2024-01-17 22:45:13 +0200

    trying to deploy to render

--- server.js
+++ server.js
@@ -33,6 +33,18 @@ app.use('/blurays', bluraysRoutes);
 app.use('/orders', orderRoutes);
 app.use('/users', userRoutes);
 
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve();
+  app.use('/uploads', express.static('/var/data/uploads'));
+  app.use(express.static(path.join(__dirname, '/front-end/build')));
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'front-end', 'build', 'index.html')));
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
+
 app.use(notFound);
 app.use(errorHandler);
 
