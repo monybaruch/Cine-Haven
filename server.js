45da2789eb007426df8512a30aa7340108cd14f1
מוני ברוך <monybaruch@gmail.com>
2024-01-17 22:51:42 +0200

    fixing a bug

--- server.js
+++ server.js
@@ -34,8 +34,6 @@ app.use('/orders', orderRoutes);
 app.use('/users', userRoutes);
 
 if (process.env.NODE_ENV === 'production') {
-  const __dirname = path.resolve();
-  app.use('/uploads', express.static('/var/data/uploads'));
   app.use(express.static(path.join(__dirname, '/front-end/build')));
 
   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'front-end', 'build', 'index.html')));
